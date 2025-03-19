78acfa212e3efc959af723a875c51095fa2d5220
yjk490 <yjk490@gmail.com>
2025-03-19 23:36:21 +0900

    stock-bot init

---
+++ src/test/java/io/yjk/stockbot/StockbotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.yjk.stockbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
